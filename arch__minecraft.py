8bb4ab4f8b1d053cb052477dff137fd68c522571
Rafayel Latif <Rafayel.latif@gmail.com>
2024-11-06 15:41:58 +0000

    bug fix

--- arch__minecraft.py
+++ arch__minecraft.py
@@ -16,7 +16,7 @@ description = "Basic Recommender System"
 #genre, length
 arch_i = [1, 1, 1]   
 arch_z = [1,1 ]           
-arch_c = []           
+arch_c = [1]           
 connector_function = "full_conn"
 
 # To maintain compatibility with our API, do not change the variable name "Arch" or the constructor class "ao.Arch" in the line below (the API is pre-loaded with a version of the Arch class in this repo's main branch, hence "ao.Arch")
